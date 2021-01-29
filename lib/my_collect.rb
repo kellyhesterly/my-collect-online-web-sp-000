138d2d2bf7d53d076a4d4acbfdbb78e9b0770c65
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-29 22:56:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect(collection)
   new_array = []
-  counter = 0
-  if counter < collection.length
-  collection.collect do |language|
-    new_array << language.upcase
-    counter += 1
-      end
-  new_array
+  i = 0
+  while condition
+    
+  end i < collection.length
+    new_array << yield(collection[i])
+    i += 1
   end
+  new_array
 end
