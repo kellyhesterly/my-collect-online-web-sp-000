549bd72965ca87eaba30ce7fd5c35106830712a7
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-29 22:44:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,3 @@
-
-
 def my_collect(collection)
   new_array = []
   counter = 0
@@ -7,13 +5,7 @@ def my_collect(collection)
   collection.collect do |language|
     new_array << language.upcase
     counter += 1
-  unless collection.split(" ").length < 1
-    collection.collect do |name|
-      new_array << name.split(" ").first
-      counter += 1
-    end
       end
   new_array
   end
-end
 end
